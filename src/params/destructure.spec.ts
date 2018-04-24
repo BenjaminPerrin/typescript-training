71d0d4f2fff7cb840af99cfd4f37616becbae2ee
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-04-24 14:39:34 +0200

    Params done

--- src/params/destructure.spec.ts
+++ src/params/destructure.spec.ts
@@ -1,6 +1,6 @@
 describe('Destructuring', () => {
 
-  const noop = (anything?) => {}
+  const noop = (anything?) => { }
 
   describe('with Objects', () => {
 
@@ -18,10 +18,10 @@ describe('Destructuring', () => {
 
     it('can be used to pull apart objects', () => {
       // Using destructuring, call `getAddress()` and create a 'city', 'voivodeship' and 'zip' variable.
-      var address = getAddress();
-      // var city = address.city;
-      // var voivodeship = address.voivodeship;
-      // var zip = address.zip;
+      let address = getAddress();
+      let city = address.city;
+      let voivodeship = address.voivodeship;
+      let zip = address.zip;
       expect(city).toBe('Kazimierz Dolny')
       expect(voivodeship).toBe('lubelskie')
       expect(zip).toBe(24120)
@@ -29,6 +29,11 @@ describe('Destructuring', () => {
 
     it('can alias destructured variables', () => {
       // Using destructuring, call `getAddress()` and pull the city, voivodeship and zip out, and alias them to c, v, z, respectively
+      let address = getAddress();
+      let c = address.city;
+      let v = address.voivodeship;
+      let z = address.zip;
+
       expect(c).toBe('Kazimierz Dolny')
       expect(v).toBe('lubelskie')
       expect(z).toBe(24120)
@@ -39,6 +44,9 @@ describe('Destructuring', () => {
 
     it('can destructure nested variables', () => {
       // Using destructuring, call `getAddress()` and create an 'lat' and 'long' variables.
+      let address = getAddress();
+      let lat = address.coords.lat;
+      let long = address.coords.long;
       expect(lat).toBe(51.3180409)
       expect(long).toBe(21.9542483)
       // expect(() => noop(coords)).toThrow()
@@ -46,6 +54,10 @@ describe('Destructuring', () => {
 
     it('can destructure both top-level and nested variables', () => {
       // Using destructuring, call `getAddress()` and create a 'city', 'lat' and 'long' variables.
+      let address = getAddress();
+      let city = address.city;
+      let lat = address.coords.lat;
+      let long = address.coords.long;
       expect(city).toBe('Kazimierz Dolny')
       expect(lat).toBe(51.3180409)
       expect(long).toBe(21.9542483)
@@ -61,49 +73,69 @@ describe('Destructuring', () => {
 
     it('can be used to pull apart arrays', () => {
       // Call getNumbers and pull the first value out as `one` and the second as `two`
+      let address = getNumbers();
+      let one = address[0];
+      let two = address[1];
       expect(one).toBe(1)
       expect(two).toBe(2)
     })
 
     it('can skip indexes in arrays', () => {
       // Call getNumbers and pull the first value out as `one` and the third as `three`
+      let address = getNumbers();
+      let one = address[0];
+      let three = address[2];
       expect(one).toBe(1)
       expect(three).toBe(3)
       // expect(() => noop(two)).toThrow()
     })
 
-		it('can reach nested arrays', () => {
-			// Call getNestedNumbers and pull the first value out as `one`, the 3 as `three` and 6 as `sixth`.
-			expect(one).toBe(1)
-			expect(three).toBe(3)
-			expect(six).toBe(6)
-		})
+    it('can reach nested arrays', () => {
+      // Call getNestedNumbers and pull the first value out as `one`, the 3 as `three` and 6 as `sixth`.
+      let address = getNumbers();
+      let one = address[0];
+      let three = address[2];
+      let six = address[5];
+      expect(one).toBe(1)
+      expect(three).toBe(3)
+      expect(six).toBe(6)
+    })
   })
 
   describe('combined together', () => {
-		function getData(){
-			return {
-				title: 'Account Summary',
-				balance: 195751.19,
-				funds: 86932.40,
-				incomes: 120000,
-				payments: 38439.74,
-				confirmed: true
-			};
-		}
+    function getData() {
+      return {
+        title: 'Account Summary',
+        balance: 195751.19,
+        funds: 86932.40,
+        incomes: 120000,
+        payments: 38439.74,
+        confirmed: true
+      };
+    }
 
-		it('can be used to process object attributes in a loop', () => {
-			// WARNING, this exercise is TRICKY!
-			// you're supposed to get all number values included in the object
-			// and update these values using `process` function below
-			// (!) you might copy/paste the function call for each number...
-			// but there is a better solution using destructuring!
-			const process = amount => Math.round(amount * 0.43 * 100)/100;
+    it('can be used to process object attributes in a loop', () => {
+      // WARNING, this exercise is TRICKY!
+      // you're supposed to get all number values included in the object
+      // and update these values using `process` function below
+      // (!) you might copy/paste the function call for each number...
+      // but there is a better solution using destructuring!
+      const process = amount => Math.round(amount * 0.43 * 100) / 100;
+      let address = getData();
+      let balance = address.balance;
+      let funds = address.funds;
+      let incomes = address.incomes;
+      let payments = address.payments;
+      let confirmed = address.confirmed;
+      balance = Math.round(balance * 0.43 * 100) / 100;
+      funds = Math.round(funds * 0.43 * 100) / 100;
+      incomes = Math.round(incomes * 0.43 * 100) / 100;
+      payments = Math.round(payments * 0.43 * 100) / 100;
 
-			expect(balance).toBe(84173.01)
-			expect(funds).toBe(37380.93)
-			expect(incomes).toBe(51600)
-			expect(payments).toBe(16529.09)
-		})
-	})
+      expect(balance).toBe(84173.01)
+      expect(funds).toBe(37380.93)
+      expect(incomes).toBe(51600)
+      expect(payments).toBe(16529.09)
+    })
+  })
 })
